bd1827c20ec97558700c4adf382338eb4fa2b433
cstuart756 <stujjc@yahoo.co.uk>
2026-01-26 15:27:51 +0000

    delete files

--- accounts/views.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.views import LoginView
-from .forms import EmailOrUsernameAuthenticationForm
-
-class CustomLoginView(LoginView):
-    template_name = "accounts/login.html"
-    authentication_form = EmailOrUsernameAuthenticationForm
