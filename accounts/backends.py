a4acc006592f00dd9ddb0635bb9198254e0d5853
cstuart756 <stujjc@yahoo.co.uk>
2026-01-26 16:06:17 +0000

    feat(auth): support login with email or username while retaining django auth urls

---
+++ accounts/backends.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+UserModel = get_user_model()
+
+class EmailOrUsernameModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None or password is None:
+            return None
+
+        identifier = username.strip()
+        if not identifier:
+            return None
+
+        try:
+            user = UserModel.objects.get(Q(username__iexact=identifier) | Q(email__iexact=identifier))
+        except UserModel.DoesNotExist:
+            return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
